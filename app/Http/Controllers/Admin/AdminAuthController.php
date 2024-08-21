568eb58b94183768179557da4990459af4816aa6
waarrdii <waarrdii@gmail.com>
2024-08-21 15:16:33 +0700

    controller done

---
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AdminAuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return Inertia::render('Admin/Auth/Login');
+    }
+    public function login(Request $request){
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+            'is_admin' => 1])){
+                return redirect()->route('admin.dashboard');//redirect to admin dashboard
+            }
+            return redirect()->route('admin.login')->with('error', 'Invalid email or password');
+    }
+
+    public function logout(Request $request){
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('admin.login');
+    }
+}
